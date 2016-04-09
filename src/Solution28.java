a3070bec12a0564f4b7cc5a74c0930c02c2135cb
Bing <bingx927@163.com>
2016-04-09 11:18:33 +0100

    Accepted.

---
+++ src/Solution28.java
@@ -0,0 +1,25 @@
+/**
+ * Created by X on 09/04/2016.
+ * Implement strStr().
+
+ Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+ */
+public class Solution28 {
+    public int strStr(String haystack, String needle) {
+        if(haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for(int i = 0; i <= haystack.length() - needle.length(); i ++) {
+            int j = 0;
+            for(int pos = i; j < needle.length() && haystack.charAt(pos) == needle.charAt(j); j ++, pos ++);
+            if(j == needle.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
